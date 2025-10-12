105d39d93cbf204825f60bbd965d470ce0f1b34e
Aleksey Gudulin <gudulinmail@gmail.com>
2025-10-12 23:34:06 +0300

    Find images in folder

--- console_app/src/application.cpp
+++ console_app/src/application.cpp
@@ -28,13 +28,34 @@ void Application::LoadSharedLibrary() {
 }
 
 void Application::FindImageFiles() {
-    for (const auto& dirEntry : fs::recursive_directory_iterator(root_path_))
-        std::cout << dirEntry << std::endl;
+    const std::vector<std::string> image_extentions = {".jpg", ".jpeg", ".jpe", ".png", ".bmp", ".jp2", ".ppm", ".sr", ".ras", ".tiff", ".tif"};
+    for (const auto& entry : fs::recursive_directory_iterator(root_path_)) {
+        if (entry.is_regular_file()) {
+            std::string extention = entry.path().extension();
+            std::transform(extention.begin(),                   //file extention is case insesetive
+                extention.end(),                                // convert extention to lowercase
+                extention.begin(),
+                [](unsigned char c){ return std::tolower(c);}
+            );
+            bool is_img = false;
+            for (const auto& ext : image_extentions) {
+                if (ext == extention) {
+                    is_img = true;
+                    break;
+                }
+            }
+            if (is_img) {
+                image_paths_.push_back(entry);
+            }
+        }
+    }
 }
 
 void Application::ProcessImages() {
-    int count = detect_face_ptr_("", faceBuf_, sizeof(faceBuf_)/sizeof(FaceRect));
-    std::cout<<"faces count "<<count<<std::endl;
+    for(const auto& img_path : image_paths_) {
+        std::cout << "img:" << img_path << std::endl;
+        int count = detect_face_ptr_(img_path.c_str(), faceBuf_, sizeof(faceBuf_)/sizeof(FaceRect));
+    }
 }
 
 void Application::Run() {
